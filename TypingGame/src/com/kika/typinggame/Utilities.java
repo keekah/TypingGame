224e4f77aa4874c79d70b9d23518d3f951c6b5fa
keekah <kika.wingert@gmail.com>
2020-08-04 12:35:42 -0700

    add Utilities class

---
+++ TypingGame/src/com/kika/typinggame/Utilities.java
@@ -0,0 +1,31 @@
+package com.kika.typinggame;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.io.File;
+import java.io.IOException;
+
+public class Utilities
+{
+
+	public static Font createFont(String filename)
+	{
+		Font font = null;
+		
+		try
+		{
+			font = Font.createFont(Font.TRUETYPE_FONT, new File(filename)).deriveFont(36f);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (FontFormatException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return font;
+	}
+
+}
